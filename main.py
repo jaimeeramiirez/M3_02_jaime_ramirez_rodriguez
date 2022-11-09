0d31a1035202d214fcc3bfeba0a0e81dcfb43a1e
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-09 16:00:37 +0000

    codigo 2 evaluado

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ except ZeroDivisionError:
     print("No se ha podido realizar la división")
 
 
-#El error en este primer código es que no podemos dividir un número entre 0 ya que no está definido. Al ejecutarlo nos saltará el error "ZeroDivisionError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del except
+#El error en este primer código es que no podemos dividir un número entre 0 ya que no está definido. Al ejecutarlo nos saltará el error "ZeroDivisionError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del "except"
 
+print("\n\n")
   
 # 2) Código a evaluar:
 lista = [4, 7, 30, 23, 5]
-lista[10]
+
+try:
+  lista[10]
+except IndexError:
+  print("El índice de la lista se encuentra fuera del rango")
+  
