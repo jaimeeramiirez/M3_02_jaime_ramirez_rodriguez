a433cbf1c27a33e57ff6065ef65e1a73c150c23a
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-09 16:06:39 +0000

    código 3 evaluado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 1) Código a evaluar:
-
 x=7
 y=0
+
 try:
   numero=7/0
 except ZeroDivisionError:
@@ -11,7 +11,8 @@ except ZeroDivisionError:
 #El error en este primer código es que no podemos dividir un número entre 0 ya que no está definido. Al ejecutarlo nos saltará el error "ZeroDivisionError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del "except"
 
 print("\n\n")
-  
+
+
 # 2) Código a evaluar:
 lista = [4, 7, 30, 23, 5]
 
@@ -22,3 +23,14 @@ except IndexError:
 
 
 #El error en este primer código es que no podemos encontrar la posición 11 de ls lista  ya que no está definida. Al ejecutarlo nos saltará el error "IndexError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del "except"
+
+print("\n\n")
+
+  
+# 3) Código a evaluar:
+paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
+
+try:
+  paises["alemania"]
+except KeyError:
+  print("Clave no encontrada dentro del diccionario")
