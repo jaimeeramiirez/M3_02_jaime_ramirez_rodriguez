fabf2b94662181d1d25adc26fecfaa5adf0b0ba3
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-09 15:57:13 +0000

    codigo 1 evaluado

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1) Código a evaluar:
+
+x=7
+y=0
+try:
+  numero=7/0
+except ZeroDivisionError:
+    print("No se ha podido realizar la división")
+
+
+#El error en este primer código es que no podemos dividir un número entre 0 ya que no está definido. Al ejecutarlo nos saltará el error "ZeroDivisionError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del except
+
+  
+# 2) Código a evaluar:
+lista = [4, 7, 30, 23, 5]
+lista[10]
