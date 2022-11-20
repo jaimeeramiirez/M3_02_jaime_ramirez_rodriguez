2b5af6a06b6cebf7d2e5b842dcb3039c176bdbc7
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-20 11:45:59 +0000

    he metido los tres primeros códigos en funciones

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ print("\n\n")
 
 # 1) Código a evaluar:
 
-x=7
-y=0
+def division(x,y):
+  try:
+    solucion= x/y
+  except ZeroDivisionError:
+      print("*No se ha podido realizar la división*")
+  else:
+    print(solucion)
 
-try:
-  numero=7/0
-except ZeroDivisionError:
-    print("*No se ha podido realizar la división*")
+  return
 
+division(7,0)
+division(7,1)
 
 #El error en este primer código es que no podemos dividir un número entre 0 ya que no está definido. Al ejecutarlo nos saltará el error "ZeroDivisionError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del "except"
 print("\n\n")
@@ -20,11 +24,17 @@ print("\n\n")
 # 2) Código a evaluar:
 
 lista = [4, 7, 30, 23, 5]
+def posicion_lista(num):
+  try:
+    lista[num]
+  except IndexError:
+    print("*El índice de la lista se encuentra fuera del rango*")
+  else:
+    print("Se encuentra en el rango de la lista")
+  return
 
-try:
-  lista[10]
-except IndexError:
-  print("*El índice de la lista se encuentra fuera del rango*")
+posicion_lista(7)
+posicion_lista(2)
 
 
 #El error en este segundo código es que no podemos encontrar la posición 11 de ls lista  ya que no está definida. Al ejecutarlo nos saltará el error "IndexError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del "except"
@@ -36,12 +46,17 @@ print("\n\n")
 # 3) Código a evaluar:
 
 paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
+def diccionario(pais):
+  try:
+    paises[pais]
+  except KeyError:
+    print("*Clave no encontrada dentro del diccionario*")
+  else:
+    print("Sí se encuentra en el diccionario; el idioma del país introducido es el: {}".format(paises[pais]))
 
-try:
-  paises["alemania"]
-except KeyError:
-  print("*Clave no encontrada dentro del diccionario*")
-
+  return
+diccionario("alemania")
+diccionario("españa")
 
 #El error en este tercer código es que no podemos encontrar la clave alemania dentro del diccionario, por consiguiente no podemos encontrar su valor; ya que ninguno de los dos está definido. Al ejecutarlo nos saltará el error "KeyError"; para solucinarlo utilizaremos "try" y "except". Dentro de la función "try" introduciremos aquello que queremos probar; si el resultado no se puede ejecutar directamente pasará al "except" donde, si el tipo de error, es el que nosotros hemos indicado se imprimirá por pantalla aquello que hay dentro del "except"
 print("\n\n")
